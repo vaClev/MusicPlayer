8eb564ebe1ac9bd1e2de7074eb865eb4732919f1
Oleg Vasilev <vasilyev_oo@ascon.ru>
2026-03-13 01:54:57 +0300

    Первая проба создания setup.exe, Работает. Но нужно пересобрать с учетом проблем в properties, и нормально назвать его

--- Windows BUILD/start.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-"C:\Program Files\Eclipse Adoptium\jdk-21.0.9.10-hotspot\bin\java.exe" --module-path "javafx-sdk-21.0.9\lib" --add-modules javafx.controls,javafx.fxml,javafx.media -jar musicpro-1.0.jar
-pause
